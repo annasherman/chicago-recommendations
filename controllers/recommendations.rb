3245aa4c8688d5d1d2fa08a0cd40afbc7529ab9e
Anna Sherman <sherman.anna@gmail.com>
2015-12-01 16:10:51 -0600

    this is almost working

--- controllers/recommendations.rb
+++ controllers/recommendations.rb
@@ -4,14 +4,34 @@
 
 class RecommendationsController < ApplicationController
 ##inherits bundler and gems
-## inherits active record and not found. 
+## inherits active record and not found.
 get '/' do
   @recommendations = Recommendations.all
-  erb :index
+  erb :read_rec
 end
 
 get '/api' do
   Recommendations.all.to_json
+end
+
+get '/api/:id' do
+  p params
+  return Recommendations.find(params[:id]).to_json
+end
+
+get '/create' do
+ erb :create_rec
+end
+
+post '/create' do
+  p params
+  @rec = Recommendations.new
+  @rec.contributor = params[:contributor]
+  @rec.place = params[:place]
+  @rec.notes = params[:notes]
+  @rec.type_of_place = params[:type_of_place]
+  @rec.save
+  erb :index
 end
 
 
